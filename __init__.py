b8f32411c8f6d530a5edabaa334824166da26281
lucasao98 <lucasacramento32@gmail.com>
2022-09-10 11:48:55 -0300

    Alterações - main

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from screens.menuClass import *
 
 if __name__ == '__main__':
 
-    # 
     menu = Menu()
     menu.setTitulo(['#############################################\n', 
     '\tBem Vindo Ao Sistema De Votação\t', 
