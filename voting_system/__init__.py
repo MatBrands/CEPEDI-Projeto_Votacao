6198458c3113654ae1b64327eb705267a9383980
MatBrands_Acer <matheusmibrandao@gmail.com>
2023-01-28 23:39:23 -0300

    Updating

--- voting_system/__init__.py
+++ voting_system/__init__.py
@@ -1,9 +1,7 @@
-from interface.models import *
-from interface.menuClass import *
-from interface.usersClass import *
-from interface.votacaoClass import *
-from models.module_CRUD import *
+import sys
+sys.path.insert(1, '../')
 
+from voting_system.view.not_yet_interface import *
 
 if __name__ == '__main__':
     tela_inicial()
